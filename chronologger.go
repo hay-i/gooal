631cb9b6cb92fcea18c4031e776b806945e6642c
Hayden Rouille <hayden@rouille.net>
2024-01-31 13:12:40 +0000

    Start server

--- chronologger.go
+++ chronologger.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
