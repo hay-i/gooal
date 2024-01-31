4399e389ec22a0fe2426252bdae2104e0eb88adf
Hayden Rouille <hayden@rouille.net>
2024-01-31 13:27:18 +0000

    Add base templ template

--- chronologger.go
+++ chronologger.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+    component := hello("World")
     e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+    e.GET("/", func(c echo.Context) error {
+        return component.Render(c.Request().Context(), c.Response().Writer)
+    })
 	e.Logger.Fatal(e.Start(":1323"))
 }
