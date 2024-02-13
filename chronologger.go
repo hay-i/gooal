098edd8f9b40967cbec2fbaf3ea9d1a4d24ae242
Igor Artemenko <igor@codemonium.com>
2024-02-13 19:08:28 +0000

    Set up, connect to, and try MongoDB

--- chronologger.go
+++ chronologger.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+	"context"
+	"time"
+
+	"github.com/jaswdr/faker"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-    component := hello("World")
-    e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return component.Render(c.Request().Context(), c.Response().Writer)
-    })
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOpts := options.Client().ApplyURI("mongodb://root:example@localhost:27017")
+	client, err := mongo.Connect(ctx, clientOpts)
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err != nil {
+		panic(err)
+	}
+
+	collection := client.Database("chronologger").Collection("goals")
+	fake := faker.New()
+	title := fake.Address().Address()
+	goal := Goal{Title: title, CreatedAt: time.Now()}
+	collection.InsertOne(ctx, goal)
+
+	var result Goal
+	findOptions := options.FindOne().SetSort(bson.D{{Key: "created_at", Value: -1}})
+	collection.FindOne(ctx, bson.D{}, findOptions).Decode(&result)
+
+	component := hello(result.Title)
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return component.Render(c.Request().Context(), c.Response().Writer)
+	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
