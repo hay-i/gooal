34873d25f01e8576f7ea6e2e265c698a17bdc30d
Hayden Rouille <hayden@rouille.net>
2024-02-26 16:36:10 +0000

    set answers when submitting form, use a transaction

--- routing/router.go
+++ routing/router.go
@@ -1,15 +1,19 @@
 package routing
 
 import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/hay-i/chronologger/components"
 	"github.com/hay-i/chronologger/db"
+	"github.com/hay-i/chronologger/models"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func Initialize(e *echo.Echo, database *mongo.Database) {
+func Initialize(e *echo.Echo, client *mongo.Client) {
+	database := client.Database("chronologger")
+
 	e.Use(middleware.Logger())
 
 	e.Static("/static", "assets")
@@ -42,10 +46,50 @@ func Initialize(e *echo.Echo, database *mongo.Database) {
 	})
 
 	e.POST("templates/:id/response", func(c echo.Context) error {
-		// TODO: Create an answer for each question
-		requestContext := c.Request().Context()
-		id := c.Param("id")
-		template := db.GetTemplate(requestContext, database, id)
+		answersCollection := database.Collection("answers")
+		templateId := c.Param("id")
+		req := c.Request()
+		requestContext := req.Context()
+
+		err := req.ParseForm()
+		if err != nil {
+			panic(err)
+		}
+
+		session, err := client.StartSession()
+		if err != nil {
+			panic(err)
+		}
+		defer session.EndSession(requestContext)
+		err = session.StartTransaction()
+		if err != nil {
+			panic(err)
+		}
+		defer session.AbortTransaction(requestContext)
+		defer session.CommitTransaction(requestContext)
+
+		templateObjectId, err := primitive.ObjectIDFromHex(templateId)
+		if err != nil {
+			panic(err)
+		}
+
+		for questionId, value := range req.Form {
+			questionObjectId, err := primitive.ObjectIDFromHex(questionId)
+			if err != nil {
+				panic(err)
+			}
+			_, insertErr := answersCollection.InsertOne(requestContext, models.Answer{
+				TemplateID: templateObjectId,
+				QuestionID: questionObjectId,
+				Answer:     value[0],
+			})
+
+			if insertErr != nil {
+				panic(insertErr)
+			}
+		}
+
+		template := db.GetTemplate(requestContext, database, templateId)
 		component := components.Start(template)
 
 		return component.Render(requestContext, c.Response().Writer)
