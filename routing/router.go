d5eedcef0a145a6e04ffc12f2cb53a88eb7cefe1
Hayden Rouille <hayden@rouille.net>
2024-03-03 19:54:39 +0000

    Restructure some controller actions

--- routing/router.go
+++ routing/router.go
@@ -1,15 +1,9 @@
 package routing
 
 import (
-	"fmt"
-	"net/http"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 
-	"github.com/hay-i/chronologger/components"
-	"github.com/hay-i/chronologger/db"
-	"github.com/hay-i/chronologger/models"
+	"github.com/hay-i/chronologger/controllers"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -21,97 +15,11 @@ func Initialize(e *echo.Echo, client *mongo.Client) {
 
 	e.Static("/static", "assets")
 
-	e.GET("/", func(c echo.Context) error {
-		requestContext := c.Request().Context()
-		component := components.Home()
+	e.GET("/", controllers.Home(database))
 
-		return component.Render(requestContext, c.Response().Writer)
-	})
-
-	e.GET("templates", func(c echo.Context) error {
-		requestContext := c.Request().Context()
-		templates := db.GetDefaultTemplates(requestContext, database)
-		component := components.Templates(templates)
-
-		return component.Render(requestContext, c.Response().Writer)
-	})
-
-	e.GET("templates/:id", func(c echo.Context) error {
-		requestContext := c.Request().Context()
-		id := c.Param("id")
-		template := db.GetTemplate(requestContext, database, id)
-		answers := db.GetAnswers(requestContext, database, id)
-		success := c.QueryParam("success")
-		component := components.Template(template, answers, success == "true")
-
-		return component.Render(requestContext, c.Response().Writer)
-	})
-
-	e.GET("templates/:id/modal", func(c echo.Context) error {
-		fmt.Println("ligma")
-		requestContext := c.Request().Context()
-		id := c.Param("id")
-		template := db.GetTemplate(requestContext, database, id)
-		component := components.Modal(template)
-
-		return component.Render(requestContext, c.Response().Writer)
-	})
-
-	e.GET("templates/:id/start", func(c echo.Context) error {
-		requestContext := c.Request().Context()
-		id := c.Param("id")
-		template := db.GetTemplate(requestContext, database, id)
-		component := components.Start(template)
-
-		return component.Render(requestContext, c.Response().Writer)
-	})
-
-	e.POST("templates/:id/response", func(c echo.Context) error {
-		answersCollection := database.Collection("answers")
-		templateId := c.Param("id")
-		req := c.Request()
-		requestContext := req.Context()
-
-		err := req.ParseForm()
-		if err != nil {
-			panic(err)
-		}
-
-		session, err := client.StartSession()
-		if err != nil {
-			panic(err)
-		}
-		defer session.EndSession(requestContext)
-		err = session.StartTransaction()
-		if err != nil {
-			panic(err)
-		}
-		defer session.AbortTransaction(requestContext)
-		defer session.CommitTransaction(requestContext)
-
-		templateObjectId, err := primitive.ObjectIDFromHex(templateId)
-		if err != nil {
-			panic(err)
-		}
-
-		for questionId, value := range req.Form {
-			questionObjectId, err := primitive.ObjectIDFromHex(questionId)
-			if err != nil {
-				panic(err)
-			}
-			_, insertErr := answersCollection.InsertOne(requestContext, models.Answer{
-				TemplateID: templateObjectId,
-				QuestionID: questionObjectId,
-				Answer:     value[0],
-			})
-
-			if insertErr != nil {
-				panic(insertErr)
-			}
-		}
-
-		// Ideally I wanted to send in http.StatusCreated, but it seems that the redirect doesn't work with that status code
-		// See: https://github.com/labstack/echo/issues/229#issuecomment-1518502318
-		return c.Redirect(http.StatusFound, "/templates/"+templateId+"?success=true")
-	})
+	e.GET("templates", controllers.Templates(database))
+	e.GET("templates/:id", controllers.Template(database))
+	e.GET("templates/:id/modal", controllers.Modal(database))
+	e.GET("templates/:id/start", controllers.Start(database))
+	e.POST("templates/:id/response", controllers.Response(database, client))
 }
