7eaa40140ece180326871962c5e7ffb6e859a538
Hayden Rouille <hayden@rouille.net>
2024-03-05 20:51:44 +0000

    Move jwt authentication

---
+++ routing/authentication.go
@@ -0,0 +1,31 @@
+package routing
+
+import (
+	"net/http"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/hay-i/chronologger/controllers"
+	"github.com/labstack/echo/v4"
+)
+
+func jwtAuthenticationMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		cookie, err := c.Cookie("token")
+		if err != nil {
+			return echo.NewHTTPError(http.StatusUnauthorized, "Missing or invalid token")
+		}
+
+		tokenString := cookie.Value
+		claims := &jwt.StandardClaims{}
+
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return []byte(controllers.SecretKey), nil
+		})
+
+		if err != nil || !token.Valid {
+			return echo.NewHTTPError(http.StatusUnauthorized, "Invalid or expired token")
+		}
+
+		return next(c)
+	}
+}
--- routing/routes.go
+++ routing/routes.go
@@ -1,37 +1,12 @@
 package routing
 
 import (
-	"net/http"
-
 	"go.mongodb.org/mongo-driver/mongo"
 
-	"github.com/golang-jwt/jwt"
 	"github.com/hay-i/chronologger/controllers"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
-
-func JwtAuthenticationMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		cookie, err := c.Cookie("token")
-		if err != nil {
-			return echo.NewHTTPError(http.StatusUnauthorized, "Missing or invalid token")
-		}
-
-		tokenString := cookie.Value
-		claims := &jwt.StandardClaims{}
-
-		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-			return []byte(controllers.SecretKey), nil
-		})
-
-		if err != nil || !token.Valid {
-			return echo.NewHTTPError(http.StatusUnauthorized, "Invalid or expired token")
-		}
-
-		return next(c)
-	}
-}
 
 func Initialize(e *echo.Echo, client *mongo.Client) {
 	database := client.Database("chronologger")
@@ -46,7 +21,7 @@ func Initialize(e *echo.Echo, client *mongo.Client) {
 	e.POST("/register", controllers.Register(database))
 	e.POST("/login", controllers.Login(database))
 
-	e.GET("/profile", controllers.Profile(database), JwtAuthenticationMiddleware)
+	e.GET("/profile", controllers.Profile(database), jwtAuthenticationMiddleware)
 
 	e.Static("/static", "assets")
 
