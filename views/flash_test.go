b100658c05c03a43fe875dd1d791701463a2ad73
Hayden Rouille <hayden@rouille.dev>
2024-03-16 15:27:46 +0000

    Add flash tests

--- views/flash_test.go
+++ views/flash_test.go
@@ -6,9 +6,28 @@ import (
 	"github.com/hay-i/chronologger/test"
 )
 
+func TestAddFlash(t *testing.T) {
+	t.Run("should add a flash to the context", func(t *testing.T) {
+		ctx := test.SetupEchoContext()
+
+		AddFlash(ctx, "error message", FlashError)
+
+		flashes := GetFlashes(ctx)[FlashError]
+
+		if len(flashes) != 1 {
+			t.Errorf("got %d flashes, want 1", len(flashes))
+		}
+	})
+}
+
 func TestGetFlashes(t *testing.T) {
 	t.Run("should return a map of flashes", func(t *testing.T) {
 		ctx := test.SetupEchoContext()
+
+		AddFlash(ctx, "error message", FlashError)
+		AddFlash(ctx, "warning message", FlashWarning)
+		AddFlash(ctx, "info message", FlashInfo)
+		AddFlash(ctx, "success message", FlashSuccess)
 
 		flashes := GetFlashes(ctx)
 
