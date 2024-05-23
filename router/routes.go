09479890a13ab9b0a4f92a931c442e568c1d1790
Hayden Rouille <hayden@rouille.dev>
2024-05-23 11:56:03 +0100

    Require login to get started

--- router/routes.go
+++ router/routes.go
@@ -29,11 +29,11 @@ func Initialize(client *mongo.Client, ctx context.Context) *echo.Echo {
 
 	e.GET("/logout", controllers.Logout(database), controllers.JwtAuthenticationMiddleware)
 	e.GET("/profile", controllers.Profile(database), controllers.JwtAuthenticationMiddleware)
+	e.GET("/get-started", controllers.GetStarted(database), controllers.JwtAuthenticationMiddleware)
 	// TODO: This is not routed, not displays anything. It will be addressed in
 	// https://github.com/hay-i/chronologger/issues/43
 	e.GET("/my-templates", controllers.MyTemplates(database), controllers.JwtAuthenticationMiddleware)
 
-	e.GET("/get-started", controllers.GetStarted(database))
 	e.POST("/template-questionnaire", controllers.TemplateQuestionnaire(database))
 
 	e.Static("/static", "assets")
