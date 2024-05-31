ef25747effe3510cf2a18475618d7db404577ad2
Hayden Rouille <hayden@rouille.dev>
2024-05-31 10:21:18 +0100

    Move javascript away from templates & reference HTML via data-attributes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,36 @@ document.addEventListener("click", function (e) {
     document.querySelector(`#${e.target.dataset.dismissable}`).innerHTML = "";
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Sortable(document.querySelector("[data-sortable-left]"), {
+    group: {
+      name: "shared",
+      pull: "clone",
+      put: false,
+    },
+    animation: 150,
+    ghostClass: "blue-background-class",
+  });
+
+  new Sortable(document.querySelector("[data-sortable-right]"), {
+    group: {
+      name: "shared",
+      pull: false,
+      put: true,
+    },
+    animation: 150,
+    ghostClass: "blue-background-class",
+    onAdd: async (evt) => {
+      const itemEl = evt.item;
+      const inputType = itemEl.getAttribute("data-type");
+      // Hacky way to get the response to render without `Base` page
+      const response = await fetch(
+        `/templates/builder?inputType=${inputType}`,
+        { headers: { "HX-Request": "true" } },
+      );
+      const formGroupHtml = await response.text();
+      itemEl.innerHTML = formGroupHtml;
+    },
+  });
+});
