9d9cd25c7a9d1308176bbaa508464af65f580fcd
Jonny Jones <jonathanjones@Jonathans-MacBook-Pro.local>
2024-06-04 23:08:13 +0100

    FEAT: refactor button on homepage to be anchorButton

--- components/home_templ.go
+++ components/home_templ.go
@@ -27,9 +27,9 @@ func Splash() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = button(
-			templ.Attributes{"href": templ.SafeURL("/get-started")},
-			"Get Started",
+		templ_7745c5c3_Err = anchorButton(
+			templ.Attributes{"href": "/get-started"},
+			"Anchor Button",
 			primary,
 		).Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
